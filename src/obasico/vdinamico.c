0f40835e189d16c3e8de7427596485da182335a4
Ricardo Coutinho <jrsscoutinho1@gmail.com>
2022-04-06 13:36:44 -0400

    feat(vdinamico): added vector sort

--- src/obasico/vdinamico.c
+++ src/obasico/vdinamico.c
@@ -3,91 +3,166 @@
  *
  *  Created on: 18 de out de 2017
  *      Author: cesar
+ *  Alterações adicionadas:
+ *   + ordernaVD
  */
 
 #include "stdlib.h"
 #include "math.h"
 #include "vdinamico.h"
+#include "stdio.h" // TODO: remover depois
+#include "lista.h"
+#include "assert.h"
+#include "string.h"
 
-struct vdinamico{
-	void* *vetor;
+typedef struct
+{
+	int chave;
+	char *palavra;
+	TListaSE *paginas; // TODO: Talvez mudar para TOcorrencia
+} TPalavra;
+
+typedef struct ocorrencia
+{
+	int pagina;
+	int qtde;
+} TOcorrencia;
+
+typedef struct ocorrenciaImportancia
+{
+	double importancia;
+	TOcorrencia *ocorrencia;
+} TOcorrrenciaImportancia;
+struct vdinamico
+{
+	void **vetor;
 	int ocupacao;
 	int tamanho;
 };
 
-void trocar(void *a, void *b){
-	//printf("%d %d\n", *a, *b);
-	void *aux = a;
-	a = b;
-	b = aux;
+static void troca(void *a, void *b, size_t size)
+{
+	void *temp = malloc(size);
 
-	//printf("%d %d\n", *a, *b);
-	//return;
+	memcpy(temp, b, size);
+	memcpy(b, a, size);
+	memcpy(a, temp, size);
+
+	free(temp);
 }
 
-int tamanhoVD(TVetorDinamico *vd){
+static int particao(TVetorDinamico *vd, int esquerda, int direita, TComparadorDD comparador, int ordem)
+{
+	void *pivot = vd->vetor[direita];
+
+	int i = esquerda - 1;
+
+	for (int j = esquerda; j < direita; j++)
+	{
+		if (comparador(vd->vetor[j], pivot) == -ordem)
+		{
+			i++;
+
+			troca(&vd->vetor[i], &vd->vetor[j], sizeof(vd->vetor[i]));
+		}
+	}
+
+	troca(&vd->vetor[i + 1], &vd->vetor[direita], sizeof(vd->vetor[i + 1]));
+
+	return (i + 1);
+}
+
+static void quickSort(TVetorDinamico *vd, int esquerda, int direita, TComparadorDD comparador, int ordem)
+{
+	if (esquerda < direita)
+	{
+		int pi = particao(vd, esquerda, direita, comparador, ordem);
+
+		quickSort(vd, esquerda, pi - 1, comparador, ordem);
+
+		quickSort(vd, pi + 1, direita, comparador, ordem);
+	}
+}
+
+/**
+ * @brief ordena vetor dinâmico
+ *
+ * @param vd
+ * @param comparador
+ * @param ordem crescente = 1 | decrescente = -1
+ */
+void ordenaVD(TVetorDinamico *vd, TComparadorDD comparador, int ordem)
+{
+	quickSort(vd, 0, vd->ocupacao - 1, comparador, ordem);
+}
+
+int tamanhoVD(TVetorDinamico *vd)
+{
 	return vd->tamanho;
 }
 
-int ocupacaoVD(TVetorDinamico *vd){
+int ocupacaoVD(TVetorDinamico *vd)
+{
 	return vd->ocupacao;
 }
 
+void inserirVD(TVetorDinamico *vd, int idx, void *elem)
+{
 
-void inserirVD(TVetorDinamico *vd, int idx, void *elem){
-
-	//printf("A---%d %d\n", idx, vd->tamanho);
-	if (idx >= vd->tamanho){
-		vd->tamanho = (int)pow(2,floor(log2(idx))+1);
-		//printf("D---%d %d\n", idx, vd->tamanho);
-		vd->vetor = realloc(vd->vetor, sizeof(void*)*vd->tamanho);
+	if (idx >= vd->tamanho)
+	{
+		vd->tamanho = (int)pow(2, floor(log2(idx)) + 1);
+		vd->vetor = realloc(vd->vetor, sizeof(void *) * vd->tamanho);
 	}
 
 	vd->vetor[vd->ocupacao] = elem;
 	vd->ocupacao++;
 }
 
-void *acessarVD(TVetorDinamico *vd, int k){
+void *acessarVD(TVetorDinamico *vd, int k)
+{
 	void *elem = NULL;
-	if ((k>=0) && (k < vd->tamanho)){
+	if ((k >= 0) && (k < vd->tamanho))
+	{
 		elem = vd->vetor[k];
 	}
 	return elem;
 }
-void *removerVD(TVetorDinamico *vd, int idx){
+void *removerVD(TVetorDinamico *vd, int idx)
+{
 	void *elem = NULL;
-  if ((idx>=0) && (idx < vd->tamanho)){
+	if ((idx >= 0) && (idx < vd->tamanho))
+	{
 		elem = vd->vetor[idx];
 		vd->vetor[idx] = NULL;
 	}
-	if(elem != NULL){
+	if (elem != NULL)
+	{
 		vd->ocupacao--;
 	}
 
 	return elem;
 }
 
-void destroyVD(TVetorDinamico *vd){
-	for(int i=0;i<vd->tamanho;i++)
-		if(vd->vetor[i]!=NULL)
+void destroyVD(TVetorDinamico *vd)
+{
+	for (int i = 0; i < vd->tamanho; i++)
+		if (vd->vetor[i] != NULL)
 			free(vd->vetor[i]);
 	free(vd->vetor);
-	vd->vetor=NULL;
-	vd->tamanho=0;
-	vd->ocupacao=0;
+	vd->vetor = NULL;
+	vd->tamanho = 0;
+	vd->ocupacao = 0;
 	free(vd);
 }
 
+TVetorDinamico *criarVetorDinamico()
+{
 
-//TVetorDinamico criarVetorDinamico(){
-
-TVetorDinamico *criarVetorDinamico(){
-
-	//TVetorDinamico vd=(TVetorDinamico){.vetor = NULL, .tamanho=2,.ocupacao=0};
 	TVetorDinamico *vd = malloc(sizeof(TVetorDinamico));
-	vd->ocupacao=0;
-	vd->tamanho=2;
-	vd->vetor = malloc(sizeof(void*) * vd->tamanho);
+	vd->ocupacao = 0;
+	vd->tamanho = 2;
+	vd->vetor = malloc(sizeof(void *) * vd->tamanho);
 
 	return vd;
 }
--- src/obasico/vdinamico.h
+++ src/obasico/vdinamico.h
@@ -3,9 +3,12 @@
  *
  *  Created on: 18 de out de 2017
  *      Author: cesar
+ *
+ *  Alterações adicionadas:
+ *   + ordernaVD
  */
-
 typedef struct vdinamico TVetorDinamico;
+typedef int (*TComparadorDD)(void *a, void *b);
 
 int tamanhoVD(TVetorDinamico *vd);
 int ocupacaoVD(TVetorDinamico *vd);
@@ -15,5 +18,7 @@ void *removerVD(TVetorDinamico *vd, int idx);
 
 void inserirVD(TVetorDinamico *vd, int idx, void *elem);
 void destroyVD(TVetorDinamico *vd);
+
+void ordenaVD(TVetorDinamico *vd, TComparadorDD comparador, int ordem);
 
 TVetorDinamico *criarVetorDinamico();
